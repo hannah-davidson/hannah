89569d1c66e588f34170a7aff92a1cc4aadb571d
Hannah Davidson <hannahrosedavidson26@gmail.com>
2026-02-02 13:28:39 -0800

    done with hw2

---
+++ homework2/homework2.py
@@ -0,0 +1,50 @@
+# File: homework2.py
+
+# Your file path should look like:
+# python_decal_fa25/yourname/homework2/homework2.py
+
+# Questions (Answer these in the homework2.py file as comments):
+
+# 1) What’s the difference between Git, GitHub, and Git Bash?
+# Git is a version control system that allows for tracking of changes in the codebase. Git Bash allows people to use Git commands using shell commands. Git Hub is a centralized, online platform for hosting Git repositories.
+# 2) What’s the difference between the terminal and the command line?
+# A terminal is a text input and output environment and allows you to run a shell. A command line is a type of interface that is used to enter commands, whereas a terminal allows us to enter commands.
+# 3) How does Windows PowerShell differ from Git Bash?
+# Git Bash is used on Linux/Unix machines, while PowerShell is run on Microsoft devices. Git Bash uses plain text, while PowerShell uses objects. 
+# 4) What’s the difference between Anaconda, conda, and Python?
+# Python is a computer programming language - it is the language that the computer "speaks." Anaconda is a bundle of software containing a Python interpreter and the standard Python library. It comes with several packages that can work together. conda is a package and environment manager that comes with Anaconda; it can be used to manage Python packages and build environments with different packages contained in them.
+# 5) What is VS Code? 
+# VS Code is a software that is used for other software development.
+# 6) What is a Jupyter Notebook? How is it different from Jupyter Lab?
+# Jupyter Notebook is a web-based computational environment. It is interactive and often used for data analysis and visualization. JupyterLab is a more interactive interface that includes more features than the limited amount available in Jupyter Notebook.
+# 7) What does ~/ mean?
+# The / is just a separator, but the ~ represents the home directory.
+# 8) What’s the difference between an absolute path and a relative path?
+# An absolute path is the full path from the root directory, while a relative path is the path dependent on the directory you are currently in.
+# 9) Imagine you're in your "yourname" repo. Write the absolute and relative paths to "course_assignments/homework2".
+# Absolute: /Users/hannahdavidson/python_decal_sp26/hannah/course_assignments/homework2
+# Relative: ~/hannah/course_assignments/homework2
+# 10) What command lets you move from "course_assignments/homework2/" to "course_assignments/"?
+# cd ..
+# 11) What would rm ./ do in your current directory? (Don’t try it!)
+# This command deletes the current working directory.
+# 12) What do the following commands do?
+# git add: First step in the Saving Work cycle, this adds files to the staging area.
+# git commit: This saves your new work so that you can push the changes to GitHub. This command saves work locally.
+# git push: Saves work remotely (to internet, on GitHub).
+
+# 13) What's the difference between "git add ." and "git add <file>"?
+# "git add ." adds all the files in a given folder, while "git add <file>" only adds a specific file.
+# 14) What do "git status" and "git log -1" do?
+# "git status" gives a report of what Git sees in your repository and what the status of the files are. "git log -1" shows the most recent change.
+# 15) What’s the difference between cloning a repository and pulling from it?
+# Cloning allows you to obtain a copy of an existing GitHub repository. Pulling brings your local repository up to date with changes that were made on the remote repository; for instance, if a collaborator uploads a change to your code on your shared GitHub, you can pull their changes to your local repository and edit the most recent code on your own machine.
+# 16) What has been your most frustrating bug or error in this class so far? How did you troubleshoot or fix it?
+# The terminal in VS Code can be buggy. Simply refreshing the terminal seemed to get it to work whenever it would error out.
+# 17) What’s a question you still have? What’s something you’re confused about?
+# What is the purpose of the GitHub Desktop Application?
+# 18) Tell me a fun fact!
+# The original Costco is in San Diego, CA.
+# 19) Print your favorite math expression you've learned in Python so far. 
+# (Hint: Use print() and add a comment explaining what it does.)
+print(5**2) # This exponentiates, so it will print the result of raising 5 to the second power.
